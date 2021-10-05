e67007fe7461c18864f1a6a744339ed1ca292121
William Majoros <bmajoros@dcc-login-02.oit.duke.edu>
2021-10-05 13:47:48 -0400

    update

--- sim-rna.py
+++ sim-rna.py
@@ -11,46 +11,32 @@ from builtins import (bytes, dict, int, list, object, range, str, ascii,
 import sys
 import ProgramName
 import gzip
-
-class GuideInfo:
-    def __init__(self,guideID,enhancerID,guideWorks):
-        self.guideID=guideID
-        self.enhancerID=enhancerID
-        self.guideWorks=guideWorks
-
-def addGuide(guideID,enhancerID,guideWorks,array):
-    guides=array.get(enhancerID)
-    if(guides is None): guides=array[enhancerID]=set()
-    guides.add(GuideInfo(guideID,guideWorks))
+import numpy as np
 
 def loadGuideInfo(filename):
-    info={}
-    with open(filename,"rt") as IN:
-        for line in IN:
-            fields=line.rstrip().split()
-            (guideID,enhancerID,guideWorks)=fields
-            addGuide(guideID,enhancerID,guideWorks,info)
-    return processGuideInfo(info)
-
-def processGuideInfo(guideInfo):
     guidesInEnhancer={}
     guideWorks=set()
-    for rec in guideInfo:
-        if(rec.guideWorks): guideWorks.add(rec.guideID)
-        if(guidesInEnhancer.get(rec.enhancerID) is None):
-            guidesInEnhancer[rec.enhancerID]=set()
-        guidesInEnhancer[rec.enhancerID].add(rec.guideID)
+    with open(filename,"rt") as IN:
+        for line in IN:
+            fields=[int(x) for x in line.rstrip().split()]
+            (guideID,enhancerID,works)=fields
+            if(works>0): guideWorks.add(guideID)
+            if(guidesInEnhancer.get(enhancerID) is None):
+                guidesInEnhancer[enhancerID]=set()
+            guidesInEnhancer[enhancerID].add(guideID)
     return (guidesInEnhancer,guideWorks)
 
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=7):
-    exit(ProgramName.get()+" <guide-info-file> <guide-truth-file> <mean-RNA-count> <rna-dispersion> <effect-size> <out-rna.txt>\n   NOTE: guide-truth-file must be sorted by guide\n")
-(guideEnhancerFile,truthFile,meanRNA,phi,beta,outRNA)=sys.argv[1:]
+if(len(sys.argv)!=8):
+    exit(ProgramName.get()+" <guide-info-file> <guide-truth-file> <mean-RNA-count> <rna-dispersion> <effect-size> <out-rna.txt> <cell-perturbation.txt>\n   NOTE: guide-truth-file must be sorted by guide\n")
+(guideEnhancerFile,truthFile,meanRNA,phi,beta,outRNA,perturbationFile)=\
+    sys.argv[1:]
 meanRNA=int(meanRNA); phi=float(phi); beta=float(beta)
 
-OUT=open(outRNA,"wt")
+PERTURBATION=open(perturbationFile,"wt")
+RNA=open(outRNA,"wt")
 (guidesInEnhancer,guideWorks)=loadGuideInfo(guideEnhancerFile)
 with gzip.open(truthFile,"rt") as IN:
     IN.readline(); IN.readline(); # skip 2 header lines
@@ -63,10 +49,12 @@ with gzip.open(truthFile,"rt") as IN:
         (guideID,cellID,present)=fields
         if(cellID>numCells): numCells=cellID
         if(guideID not in guideWorks): next
-        if(guideID in guidesInEnhancer):
+        #print("guideID=",guideID,"enhancerID=",thisEnhancer,
+        #      "guidesInEnhancer=",guidesInEnhancer[thisEnhancer])
+        if(guideID in guidesInEnhancer[thisEnhancer]):
             perturbedCells.add(cellID)
         else:
-            muBeta=mu*beta
+            muBeta=meanRNA*beta
             for i in range(numCells):
                 cellID=i+1
                 mu=muBeta if cellID in perturbedCells else meanRNA
@@ -74,8 +62,15 @@ with gzip.open(truthFile,"rt") as IN:
                 P=(var-mu)/var
                 N=mu*mu/(var-mu)
                 count=np.random.negative_binomial(N,P)
-                print(gendID,cellID,count,file=OUT)
-                thisEnhancer+=1
-                geneID+=1
-                perturbedCells=set()
-            
+                #print("beta=",beta,"mu=",muBeta,"meanRNA=",meanRNA,"var=",
+                #      var,"phi=",phi,"P=",P,"N=",N,"count=",count)
+                print(geneID,cellID,count,file=RNA)
+                status=1 if cellID in perturbedCells else 0
+                print(geneID,cellID,status,file=PERTURBATION)
+            thisEnhancer+=1
+            geneID+=1
+            perturbedCells=set()
+                
+            ### need to push back the previous guide or it will get lost!
+
+                
